aba0b39485af7608039635bbe7f3618d43d90de6
SawyerMifsud <wingren013@gmail.com>
2016-10-20 18:38:12 -0700

    Update main.c

--- main.c
+++ main.c
@@ -5,7 +5,8 @@
 /*
 	this test requires you to read it to understand it's output. It's worth it though as it catches a few things the others don't.
 	obviously use it in conjunction with other tests as this is not exhaustive.
-	if you have an issue with the difficulty to use this test rmemeber that 42 rewards
+	if you have an issue with the difficulty to use this test remember that 42 rewards critical thinking and initiative. 
+	If you still disagree with my methodology then poc or gtfo
 */
 
 void	test_test()
